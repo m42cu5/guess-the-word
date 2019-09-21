bbc0e6d4796494b913fea580b25e4a9cb63a42ee
Marcus Nilsson <l.a.marcus.nilsson@gmail.com>
2019-09-21 09:18:50 +0200

    Added some comments for the future.

--- word-guess.py
+++ word-guess.py
@@ -2,21 +2,24 @@ import random
 
 words = ["apple", "banana", "orange"]
 word = random.choice(words)
+# Store the clue in a list (array) for easier manipulation.
 clue = list("_" * len(word))
 guess = ""
 nbr_of_guesses = 0
 
 while guess != word:
-    guess = input(f'Guess the word "{"".join(clue)}": ').lower()
+    # Print the clue as a string instead of a list.
+    guess = input(f'Guess the word "{"".join(clue)}":').lower()
     nbr_of_guesses += 1
     if guess != word:
         if len(guess) == 1:
             occurrences = word.count(guess)
             position = 0
             while occurrences > 0:
-                occurrences -= 1
+                # Replace the first occurrence of the guess in the clue, starting from the current position.
                 position = int(word.find(guess, position, -1))
                 clue[position] = guess
+                occurrences -= 1
                 position += 1
             guess = "".join(clue)
 print(f'''Correct! The word was "{word}".
